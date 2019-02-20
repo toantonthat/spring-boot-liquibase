33f97dbb62008b48065961917c1ffcfd64cd69bb
ToanTT <toantp.91@greenglobal.com>
2019-02-20 11:00:39 +0700

    update changelog

---
+++ src/main/java/com/toantt/liquibase/entities/Category.java
@@ -0,0 +1,47 @@
+package com.toantt.liquibase.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	private int parentId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+
+}
